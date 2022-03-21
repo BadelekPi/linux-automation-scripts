d4c510cf2d2894c795bec80f671478fde9430291
badelekpi <diuf100@gmail.com>
2022-03-21 22:28:42 +0100

    To 43 bash script

--- commands.sh
+++ commands.sh
@@ -26,4 +26,42 @@ echo expr $num1 \* $num2
 echo "$(($num1+$num2))"
 liczba="$(($num1+$num2))"
 
+# Double paranthesis to assign to variable
+var=$((num1+10))
+echo "$var"
+
+# Multiplication has precedence
+var=$((3*2+1))
+echo $var
+
+# Combine two strings in one line with variables
+var1="hello"
+var2="world"
+combine="$var1 $var2"
+echo $combine
+
+var3="beauty"
+echo combine2="${var1} ${var3} ${var2}"
+
+#Combine with plus operator
+var1+=" $var2"
+echo $var1
+
+# Create multi-line string var with HEREDOC
+string=$(cat<<'END_HEREDOC'
+I can
+type 
+in multilines
+END_HEREDOC
+)
+echo "$string"
+
+# Cat multi-line heredoc text
+cat<<LINUXHINT
+The current working directory is: $PWD
+You are logged in as: $(whoami)
+LINUXHINT
+
+
+
 
